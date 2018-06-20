25055cbda32f8491e84ab41262f19fa206bfa8d0
Trombetta <Sebastiano.Trombetta@wolterskluwer.com>
2018-06-20 17:11:05 +0200

    Added more option tests.

--- Tests/Trombetta.Cli.Test/CommandLine/OptionTest.cs
+++ Tests/Trombetta.Cli.Test/CommandLine/OptionTest.cs
@@ -8,6 +8,7 @@ namespace Trombetta.Cli.Test.CommandLine
 {
    public class OptionTest
    {
+
       [Fact]
       public void SuccessfullyCreateOptionWithSingleAlias()
       {
@@ -16,11 +17,29 @@ namespace Trombetta.Cli.Test.CommandLine
       }
 
       [Fact]
-      public void SuccessfullyCreateOptionWithMultipleAliases()
+      public void OptionCannotHaveEmptyAlias()
+      {
+         Action action = () => new Option(new[] { "" }, "");
+         Assert.Throws<ArgumentException>(action);
+      }
+
+      [Fact]
+      public void OptionHasMultipleAliases()
+      {
+         var option = new Option(new[] { "option", "opt", "o" }, "");
+         Assert.True(option.Aliases.Count() == 3);
+         Assert.True(option.Name == "option");
+         Assert.True(option.Aliases.Contains("opt"));
+         Assert.True(option.Aliases.Contains("o"));
+      }
+
+      [Fact]
+      public void OptionAliasesAreCaseSensitive()
       {
          var option = new Option(new[] { "option", "o" }, "");
          Assert.True(option.Aliases.Count() == 2);
          Assert.True(option.Name == "option");
+         Assert.True(option.Aliases.Contains("O") == false);
       }
    }
 }
